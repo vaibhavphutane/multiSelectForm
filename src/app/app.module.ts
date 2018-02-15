59aa946836483be3c8f56add0a1f8b7e255a4d5c
vaibhavphutane <vap1231@gmail.com>
2018-02-16 01:56:01 +0530

    multi step form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { FormWizardModule } from 'angular2-wizard';
+import {FormsModule} from '@angular/forms';
+import { NgModel } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormWizardModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
